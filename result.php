af222c61826fb01a5f928da28e8cb2c86ac35549
gotooza <510hidepon@gmail.com>
2020-10-15 22:38:29 +0900

    add form function

---
+++ result.php
@@ -0,0 +1,14 @@
+<?php
+  echo "<p>取得中。。。" . $_GET["ripo"] ."</p>";
+
+//   echo "<p>取得中。。。" . $_GET["ripo"] ."</p>";
+//   $fp = fopen("command.data","w");
+//   fwrite($fp, $_GET["ripo"]);
+//   fclose($fp);
+
+//   echo '<pre>';
+//   $last_line = system('python out.py');
+//   $last_line
+
+?>
+
