5ef588a81d50e92cb78daf27a2fc2cad4f422b63
gotooza <510hidepon@gmail.com>
2020-10-15 23:45:15 +0900

    add action switch funciton

--- result.php
+++ result.php
@@ -1,14 +1,48 @@
+<head>
+     <style type="text/css">
+        body {
+            margin: 50px;
+        }
+    </style>
+
+  </head>
+
 <?php
-  echo "<p>取得中。。。" . $_GET["ripo"] ."</p>";
+  echo "<p>操作対象：" . $_GET["machineName"] ."</p>";
+  echo "<p>操作内容：" . $_GET["actionNum"] ."</p>";
 
-//   echo "<p>取得中。。。" . $_GET["ripo"] ."</p>";
-//   $fp = fopen("command.data","w");
-//   fwrite($fp, $_GET["ripo"]);
-//   fclose($fp);
+  //intに変換
+  $action = intval($_GET["actionNum"]);
 
-//   echo '<pre>';
-//   $last_line = system('python out.py');
-//   $last_line
+  echo '<pre>';
+    switch ($action){
+        case 1:
+            //Vagrantfileで $_GET["machineName"] を立ち上げ
+            case1();
+            break;
+        
+        case 2:
+            //ansibleで $_GET["machineName"] のバックアップを実行 => $_GET["machineName"].bootができる、gitにpush
+            case2();
+            break;
+            
+        case 3:
+            //ansibleで $_GET["machineName"] のリストアを実行
+            case3();
+            break;   
+    }
+
+    //コマンドは自身で
+    function case1() {
+       system('pwd');
+    }
+
+    function case2() {
+        system('ls -l');
+    }
+
+    function case3() {
+        system('df -h');
+    }
 
 ?>
-
